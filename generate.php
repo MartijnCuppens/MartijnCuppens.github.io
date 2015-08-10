38cf8bc28a7581b36e59480ec9689537e9aaa639
MartijnCuppens <martijn@pure-communication.be>
2015-08-10 11:59:40 +0200

    overlay

--- generate.php
+++ generate.php
@@ -12,12 +12,12 @@ Cras id dui. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula le
 'carousel' => [
 'title' => 'Carousel',
 'text' => '<div class="carousel">
-  <input type="radio" name="carousel" id="slide-1" checked>
-  <input type="radio" name="carousel" id="slide-2">
-  <input type="radio" name="carousel" id="slide-3">
-  <input type="radio" name="carousel" id="slide-4">
-  <input type="radio" name="carousel" id="slide-5">
-  <ul class="slides slides-5">
+  <input type="radio" name="carousel" id="carousel-slide-1" checked>
+  <input type="radio" name="carousel" id="carousel-slide-2">
+  <input type="radio" name="carousel" id="carousel-slide-3">
+  <input type="radio" name="carousel" id="carousel-slide-4">
+  <input type="radio" name="carousel" id="carousel-slide-5">
+  <ul class="carousel-slides carousel-slides-5">
   <li>
   <img src="http://lorempixel.com/600/340/abstract/1" height="340" width="600" alt="Slide 1">
   </li>
@@ -34,48 +34,177 @@ Cras id dui. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula le
   <img src="http://lorempixel.com/600/340/abstract/5" height="340" width="600" alt="Slide 5">
   </li>
   </ul>
-  <ul class="dots">
+  <ul class="carousel-dots">
   <li>
-  <label for="slide-1">Slide 1</label>
+  <label for="carousel-slide-1">Slide 1</label>
   </li>
   <li>
-  <label for="slide-2">Slide 2</label>
+  <label for="carousel-slide-2">Slide 2</label>
   </li>
   <li>
-  <label for="slide-3">Slide 3</label>
+  <label for="carousel-slide-3">Slide 3</label>
   </li>
   <li>
-  <label for="slide-4">Slide 4</label>
+  <label for="carousel-slide-4">Slide 4</label>
   </li>
   <li>
-  <label for="slide-5">Slide 5</label>
-  </li>
-  </ul>
-  <ul class="arrows">
-  <li>
-  <label class="prev" for="slide-5">Previous</label>
-  <label class="next" for="slide-2">Next</label>
-  </li>
-  <li>
-  <label class="prev" for="slide-1">Previous</label>
-  <label class="next" for="slide-3">Next</label>
-  </li>
-  <li>
-  <label class="prev" for="slide-2">Previous</label>
-  <label class="next" for="slide-4">Next</label>
-  </li>
-  <li>
-  <label class="prev" for="slide-3">Previous</label>
-  <label class="next" for="slide-5">Next</label>
-  </li>
-  <li>
-  <label class="prev" for="slide-4">Previous</label>
-  <label class="next" for="slide-1">Next</label>
+  <label for="carousel-slide-5">Slide 5</label>
   </li>
   </ul>
-
+  <ul class="carousel-arrows">
+  <li>
+  <label class="btn-arrow prev" for="carousel-slide-5">Previous</label>
+  <label class="btn-arrow next" for="carousel-slide-2">Next</label>
+  </li>
+  <li>
+  <label class="btn-arrow prev" for="carousel-slide-1">Previous</label>
+  <label class="btn-arrow next" for="carousel-slide-3">Next</label>
+  </li>
+  <li>
+  <label class="btn-arrow prev" for="carousel-slide-2">Previous</label>
+  <label class="btn-arrow next" for="carousel-slide-4">Next</label>
+  </li>
+  <li>
+  <label class="btn-arrow prev" for="carousel-slide-3">Previous</label>
+  <label class="btn-arrow next" for="carousel-slide-5">Next</label>
+  </li>
+  <li>
+  <label class="btn-arrow prev" for="carousel-slide-4">Previous</label>
+  <label class="btn-arrow next" for="carousel-slide-1">Next</label>
+  </li>
+  </ul>
 </div>',
 'active' => ['aboutActive', 'about1Active']
+],
+'overlay' => [
+'title' => 'Overlay',
+'text' => '
+  <label for="overlay-slide-1">Open overlay</label>
+  <div class="overlay">
+  <input type="radio" name="overlay" id="overlay-slide-1">
+  <input type="radio" name="overlay" id="overlay-slide-2">
+  <input type="radio" name="overlay" id="overlay-slide-3">
+  <input type="radio" name="overlay" id="overlay-slide-4">
+  <input type="radio" name="overlay" id="overlay-slide-5">
+  <input type="radio" name="overlay" id="overlay-slide-6">
+  <input type="radio" name="overlay" id="overlay-slide-7">
+  <input type="radio" name="overlay" id="overlay-slide-8">
+  <input type="radio" name="overlay" id="overlay-close" class="overlay-close">
+  <div class="overlay-inner">
+  <ul class="overlay-slides overlay-slides-8">
+  <li>
+  <div class="overlay-item">
+  <img src="http://lorempixel.com/1200/900/abstract/1" width="900"height="1200" alt="Slide 1">
+  <span class="title">Slide 1</span>
+  </li>
+  <li>
+  <div class="overlay-item">
+  <img src="http://lorempixel.com/1600/800/abstract/2" width="1600" height="340"  alt="Slide 2">
+  <span class="title">Slide 2</span>
+  </div>
+  </li>
+  <li>
+  <div class="overlay-item">
+  <img src="http://lorempixel.com/800/450/abstract/3" width="800" height="340" alt="Slide 3">
+  <span class="title">Slide 3</span>
+  </div>
+  </li>
+  <li>
+  <div class="overlay-item">
+  <img src="http://lorempixel.com/600/340/abstract/4" width="600" height="340" alt="Slide 4">
+  <span class="title">Slide 4 - (next slide has no title)</span>
+  </div>
+  </li>
+  <li>
+  <div class="overlay-item">
+  <img src="http://lorempixel.com/1200/700/abstract/5" width="1200" height="700" alt="Slide 5">
+  </div>
+  </li>
+  <li>
+  <div class="overlay-item">
+  <img src="http://lorempixel.com/1600/900/abstract/6" width="1600" height="900" alt="Slide 6">
+  <span class="title">Slide 6</span>
+  </div>
+  </li>
+  <li>
+  <div class="overlay-item">
+  <img src="http://lorempixel.com/1600/900/abstract/6" width="1600" height="900" alt="Slide 7">
+  <span class="title">Slide 7</span>
+  </div>
+  </li>
+  <li>
+  <div class="overlay-item">
+  <img src="http://lorempixel.com/1600/900/abstract/6" width="1600" height="900" alt="Slide 8">
+  <span class="title">Slide 8</span>
+  </div>
+  </li>
+  <li class="overlay-close-item">
+  <label class="overlay-close-overlay" for="overlay-close">Close</label>
+  </li>
+  </ul>
+  <ul class="overlay-dots">
+  <li>
+  <label for="overlay-slide-1">Slide 1</label>
+  </li>
+  <li>
+  <label for="overlay-slide-2">Slide 2</label>
+  </li>
+  <li>
+  <label for="overlay-slide-3">Slide 3</label>
+  </li>
+  <li>
+  <label for="overlay-slide-4">Slide 4</label>
+  </li>
+  <li>
+  <label for="overlay-slide-5">Slide 5</label>
+  </li>
+  <li>
+  <label for="overlay-slide-6">Slide 6</label>
+  </li>
+  <li>
+  <label for="overlay-slide-7">Slide 7</label>
+  </li>
+  <li>
+  <label for="overlay-slide-8">Slide 8</label>
+  </li>
+  </ul>
+  <ul class="overlay-arrows">
+  <li>
+  <label class="btn-arrow prev" for="overlay-slide-8">Previous</label>
+  <label class="btn-arrow next" for="overlay-slide-2">Next</label>
+  </li>
+  <li>
+  <label class="btn-arrow prev" for="overlay-slide-1">Previous</label>
+  <label class="btn-arrow next" for="overlay-slide-3">Next</label>
+  </li>
+  <li>
+  <label class="btn-arrow prev" for="overlay-slide-2">Previous</label>
+  <label class="btn-arrow next" for="overlay-slide-4">Next</label>
+  </li>
+  <li>
+  <label class="btn-arrow prev" for="overlay-slide-3">Previous</label>
+  <label class="btn-arrow next" for="overlay-slide-5">Next</label>
+  </li>
+  <li>
+  <label class="btn-arrow prev" for="overlay-slide-4">Previous</label>
+  <label class="btn-arrow next" for="overlay-slide-6">Next</label>
+  </li>
+  <li>
+  <label class="btn-arrow prev" for="overlay-slide-5">Previous</label>
+  <label class="btn-arrow next" for="overlay-slide-7">Next</label>
+  </li>
+  <li>
+  <label class="btn-arrow prev" for="overlay-slide-6">Previous</label>
+  <label class="btn-arrow next" for="overlay-slide-8">Next</label>
+  </li>
+  <li>
+  <label class="btn-arrow prev" for="overlay-slide-7">Previous</label>
+  <label class="btn-arrow next" for="overlay-slide-1">Next</label>
+  </li>
+  </ul>
+</div>
+</div>',
+'active' => ['aboutActive', 'about2Active']
 ],
 'about-2' => [
 'title' => 'About 2',
@@ -173,6 +302,7 @@ foreach ($pages as $key => $page) {
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/navigation.css">
   <link rel="stylesheet" href="css/carousel.css">
+  <link rel="stylesheet" href="css/overlay.css">
 </head>
 <body>
   <!--[if IE]><p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
